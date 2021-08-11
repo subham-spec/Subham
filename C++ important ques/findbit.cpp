56cee641c64f42915b7704574240cdcc62b39aab
Subham Sharma <81345739+subham-spec@users.noreply.github.com>
2021-08-11 11:51:16 +0530

    Create findbit.cpp

---
+++ C++ important ques/findbit.cpp
@@ -0,0 +1,32 @@
+/*
+BIT MANUPULATION
+To find the bit of a number at the position
+Example:
+10110101 at 5=?
+
+Answer:
+1
+*/
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int no;
+    cout<<"Enter the number : ";
+    cin>>no;
+    
+    cout<<"Enter the position to find the bit : ";
+    int n;
+    cin>>n;
+
+    int no2=pow(2,n);
+    int i=no&no2;
+    if(i){
+        cout<<"At "<<n<<" place there is 1"<<endl;
+    }
+    else{
+        cout<<"At "<<n<<" place there is 0"<<endl;
+    }
+    
+    return 0;
+}
